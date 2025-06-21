1ce312e28b90918aadaf725ae1a3d99a92070f9c
shiva396 <suryashiva396@gmail.com>
2025-06-21 21:26:40 +0530

    new commit

---
+++ lib/data/onboard.dart
@@ -0,0 +1,27 @@
+// class OnBoard {
+//   final String imaage, title, description;
+//   const OnBoard({
+//     required this.imaage,
+//     required this.title,
+//     required this.description,
+//   });
+// }
+
+// final List<OnBoard> demoData = [
+//   OnBoard(
+//     image: AppImage.firstImage,
+//     title: "Find Best Musicians All Around Your City",
+//     description: "Thousands of musicians around you are waiting to rock your event.",
+//   ),
+//   OnBoard(
+//     image: AppImage.secondImage,
+//     title: "Fastest Way To Book Great Musicians",
+//     description: "Find the perfect match to perform for your event and make the day remarkable.",
+//   ),
+//   OnBoard(
+//     image: AppImage.thirdImage,
+//     title: "Find Top Sessions Pros For Your Event",
+//     description: "Find the perfect match to perform for your event and make the day remarkable.",
+//   ),
+// ];
+
